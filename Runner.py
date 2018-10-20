4ead9c8dc9ea1a71db5152fe0c34e35454143a0f
Unknown <sepehrhaj77@gmail.com>
2018-10-20 18:46:05 -0400

    Revert "Revert "First Working Push""
    This reverts commit 809e29f912354bc67ca7cdbd805d4aa2196bbfa2.

--- Runner.py
+++ Runner.py
@@ -1,14 +1,74 @@
-#def playerAttack(target):
-#    target.m_hp -= 1
+from Enemy import Enemy
+from Player import Player
 
-def level1():
-    #introMessage = "Printing is how you display messages on the command line! Do this by typing \"print(\"<your message>\")\" Use this to attack the python! print \"The knight attacks the python\"\n"
-    usrInput = input("Enter answer: ")
-    print(usrInput)
+#This function will handle the running of the first level
+def level1(player):
+    success = False
+    #Instantiate the snake and answer, and set success to false. Success determines if the player has beaten the level
+    snake1 = Enemy(1)
+    answerToken = "print(\"The knight attacks the python\")"
 
+    #while loop to continually play the level until the player dies or the enemy snake dies
+    while (not(player.isDead()) and not(success)):
+
+        #Give player quick tutorial on how the print statement works. Then, get their input
+        introMessage = "Printing is how you display messages on the command line! Do this by typing: print(\"<your message>\") \nHere is an example: print(\"Hello World!\")\nUse this to attack the python! Try printing the message \"The knight attacks the python\" (case-sensitive)\n"
+        usrInput = input(introMessage)
+
+        #Compare the player's answer to the correct answer:
+
+        #Player is correct
+        if(usrInput == answerToken):
+            snake1.loseHP()
+            #Check if the attack killed the snake
+            if(snake1.isDead()):
+                success = True
+                print("Snake has died.")
+        
+        #Player is wrong
+        else:
+            player.loseHP()
+        
+    #The loop will only exit if the player has died or beaten the level. Check which one here
+    if(success):
+        print("Level 1 complete! The python has been slain.")
+        
+    else:
+        print("Game Over!")
+
+    return success  
+    
+#Function to see if the player wants to restart their game after dying
+def keepPlaying():
+    answer = input("Do you wish to restart? [y/n]")
+    return answer == "y"
+
+#The main runner function
 def main():
-    print("Welcome to Slay the Python. bitch")
-    level1()
-    print("level 1 complete!")
+    #Instantiate a player with 5 hearts. 
+    p1 = Player(5)
+    print("Welcome to Slay the Python!")
+    play = True
+
+    while(play):
+        #Begin the first level. Set success for the level to false. Afterwards, check if the game is over and if they want to restart with keepPlaying
+        success = level1(p1)
+        
+        #If the player died
+        if(not success):
+            #If they wish to restart
+            if(keepPlaying()):
+                p1.refillHearts()
+                continue
+            else:
+                play = False
+                print("Game over! Goodbye.")
+        play = False
+        print("You won! u have big ghey")
+
+            
+    
+    
+    
 
 main()
