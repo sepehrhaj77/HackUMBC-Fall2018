809e29f912354bc67ca7cdbd805d4aa2196bbfa2
Unknown <sepehrhaj77@gmail.com>
2018-10-20 18:46:05 -0400

    Revert "First Working Push"
    This reverts commit 44839c2a680694c8de05f47744a54f4bc2b5a1ae.

--- Enemy.py
+++ Enemy.py
@@ -1,22 +1,3 @@
 class Enemy:
-
-    #Constructor - sets the hp of the snake to the passed parameter
-    def __init__(self, health):
-        self.hp = health
-
-    #Returns the hp of the snake
-    def getHealth(self):
-        return self.hp
+    m_hp = 1
     
-    #Makes the snake lose 1 heart   
-    def loseHP(self):
-        self.hp -= 1
-        print("A mighty blow! The snake loses a heart.")    
-
-    
-    #Returns if the snake is dead. True->Dead; False->Alive
-    def isDead(self):
-        if(self.hp == 0):
-            return True
-        else:
-            return False
--- Player.py
+++ Player.py
@@ -1,35 +1,7 @@
-MAX_PLAYER_HP = 5
-
 class Player:
-    #Constructor - sets the hp of the player to the passed parameter
-    def __init__(self, health):
-        self.hp = health
+    m_hp = 3
+    
+    #hp is the only mechanical part of the player currently
+    #everything else is front end.
 
-    #Makes the player lose 1 heart
-    def loseHP(self):
-        self.hp -= 1
-        print("Ouch! You lost a heart")    
-            
-    #Set the player's hp back to the max
-    def refillHearts(self):
-        self.hp = MAX_PLAYER_HP
-
-    #Returns the hp of the player
-    def getHealth(self):
-        return self.hp
-
-    #Replenishes one heart
-    def replenishHeart(self):
-        if(self.hp >= 5):
-            print("Full health!")
-        else:
-            self.hp += 1
-            print("One heart restored")
-
-
-    #Returns if the player is dead. True->Dead; False->Alive
-    def isDead(self):
-        if(self.hp == 0):
-            return True
-        else:
-            return False
+    #def (animation?)
