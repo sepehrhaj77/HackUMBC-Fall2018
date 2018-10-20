0b932f582af92192338ea706ddf60628c155b850
Jo Thomas <jothoma1@umbc.edu>
2018-10-20 16:36:00 -0400

    work pls

--- Runner.py
+++ Runner.py
@@ -1,13 +1,14 @@
-PLAYERBASEHP = 3
-def playerAttack(target):
-    target.m_hp -= 1
+#def playerAttack(target):
+#    target.m_hp -= 1
 
 def level1():
-    introMessage = "Printing is how you display messages on the command line! Do this by typing \"print(\"your message\")\" Use this to attack the python! print \"The knight attacks the python\""
-    print(intro message)
-    #get the user code
-    if("last thing on command line" == "The knight attacks the python")
-        success!
-    
+    #introMessage = "Printing is how you display messages on the command line! Do this by typing \"print(\"<your message>\")\" Use this to attack the python! print \"The knight attacks the python\"\n"
+    usrInput = input("Enter answer: ")
+    print(usrInput)
 
-def game:
+def main():
+    print("Welcome to Slay the Python. bitch")
+    level1()
+    print("level 1 complete")
+
+main()
