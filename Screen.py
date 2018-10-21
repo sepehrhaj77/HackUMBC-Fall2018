36deffa27b792bf93f68007e257e677b51d45027
luckindan <43219097+luckindan@users.noreply.github.com>
2018-10-21 03:56:09 -0400

    Add files via upload

--- Screen.py
+++ Screen.py
@@ -17,11 +17,11 @@ wh = 600
 
 class InputBox:
 
-    def __init__(self, x, y, w, h, FONT,  text=''):
+    def __init__(self,FONT= pygame.font.Font(None, 32),  text='',x=100, y=550, w=600, h=34):
         self.rect = pygame.Rect(x, y, w, h)
-        self.color = COLOR_INACTIVE
+        self.color = (0,0,0)
         self.text = text
-        self.txt_surface = FONT.render(text, True, self.color)
+        self.txt_surface = FONT.render(text, True, (0,0,0))
 
 
 
@@ -37,7 +37,7 @@ class InputBox:
             elif event.key == pygame.K_BACKSPACE:
                 self.text = self.text[:-1]
        
-            elif self.txt_surface.get_width() < 590:
+            elif self.txt_surface.get_width() < 580:
                 self.text += event.unicode
                 # Re-render the text.
             self.txt_surface = FONT.render(self.text, True, self.color)
@@ -52,9 +52,13 @@ class InputBox:
     def draw(self, screen):
         # Blit the text.
 
-        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        
         # Blit the rect.
-        pygame.draw.rect(screen, self.color, self.rect, 2)
+        pygame.draw.rect(screen, (255,255,255), self.rect, 0)
+        pygame.draw.rect(screen, (0,0,0), self.rect, 2)
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5)) 
+
+
 
 class Screen:
 
@@ -149,14 +153,37 @@ class Screen:
     
     def battleScreen(self, playerSprite, enemySprite, playerHearts, enemyHearts, text):
 
-        """
-        self.playerSprite = playerSprite
-        self.enemySprite = enemySprite
-        self.playerHearts = playerHearts
-        self.enemyHearts = enemyHearts
-        """
+        game = 1
 
+        #text message goes here
+        textMessage = text
+
+        mybackGround = self.backGround
+        input_box = InputBox()
+        textBox = Textprint(textMessage)
+        window.fill((COLOR_SKY))
         
+        while game == 1:
+     
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        game = 0
+
+                Input = input_box.handle_event(event)
+                if(Input):
+
+                    textBox.changeText("You did something")
+
+
+            #visual effects  
+            textBox.blit_text(window)     
+            input_box.draw(window)
+            pygame.display.flip()
+            window.blit(backGround, (0,50))
+            window.blit(playerSprite, (150, 120))
+            window.blit(enemySprite, (450, 200))
+            
 
 
 
