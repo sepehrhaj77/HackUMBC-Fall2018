d2cceb8be0f6c224229709673a330abac655fada
luckindan <43219097+luckindan@users.noreply.github.com>
2018-10-20 21:49:03 -0400

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,16 +1,112 @@
-# Zeit wird gezaehlt
+
 import pygame, math, random, time
 from pygame.locals import *
+#from inputBox import InputBox
+
+pygame.init()
+
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, FONT,  text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+
+
+    def handle_event(self, event):
+        temp = ''
+        if event.type == pygame.KEYDOWN:
+            
+            if event.key == pygame.K_RETURN:
+                print(self.text)
+                temp = self.text
+                self.text = ''
+                window.fill((30, 30, 30))
+
+
+            elif event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+                window.fill((30, 30, 30))
+
+            elif self.txt_surface.get_width() < 590:
+                self.text += event.unicode
+                # Re-render the text.
+            self.txt_surface = FONT.render(self.text, True, self.color)
+        if temp != '':
+            return temp
+
+    def update(self):
+        # Resize the box if the text is too long.
+        
+        self.rect.w = 600
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
 
 
 
-def StartGame(fenster):
-    printManual(fenster)
-  
+bg = (20, 20, 50)
+black = (0, 0, 0)
+
+
+
+ww = 800
+wh = 600
+
+
+
+def StartGame(window):
+    printManual(window)
+    level1(window)
     return 0
 
+def level1(window):
+    x2 = 1
+    FONT = pygame.font.Font(None, 32)
 
-def printManual(fenster):
+    backGround = pygame.image.load("background.png")
+    input_box = InputBox(100, 500, 600, 34 , FONT)
+    window.fill((30, 30, 30))
+    
+    while x2 == 1:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    x2 = 0
+                    game = 0
+
+            input_box.handle_event(event)
+
+     
+        input_box.update()
+        #window.fill((30, 30, 30))
+        input_box.draw(window)
+
+        pygame.display.flip()
+
+        window.blit(backGround, (50,50))
+
+        window.blit(knight, (150, 120))
+        window.blit(First, (450, 200))
+ 
+        pygame.display.update()
+
+    #while status == 1:
+def level2(window):
+    print("level2")
+
+def final(window):
+    print("Final")
+
+def printManual(window):
     bg = (255, 255, 255)
     processed = 0
     while processed == 0:
@@ -23,11 +119,11 @@ def printManual(fenster):
 
 
 
-        fenster.fill((255, 175, 0))
+        window.fill((255, 175, 0))
         text_title = pygame.font.SysFont(None, 125)
 
         title = text_title.render("Manual", True, black)
-        fenster.blit(title, (50, 50))
+        window.blit(title, (50, 50))
 
         """
         print the manual here
@@ -36,30 +132,24 @@ def printManual(fenster):
         pygame.display.update()
 
 
-pygame.init()
-
-#bg = (255, 255, 255)
-bg = (20, 20, 50)
-black = (0, 0, 0)
 
 
-
-ww = 800
-wh = 600
-
-fenster = pygame.display.set_mode((ww, wh), FULLSCREEN)
+window = pygame.display.set_mode((ww, wh), FULLSCREEN)
 pygame.mouse.set_visible(0)
-fenster.fill(bg)
+window.fill(bg)
 pygame.display.set_caption("Slay the Python")
 
 pygame.display.update()
+
+knight  = pygame.image.load("knight.png")
+First = pygame.image.load("python_blue.png")
 
 #pygame.mixer.music.load("noise.mp3")
 ########################################################################################################################################################################
 
 game = 1
 while game == 1:
-    time_count = 0
+
  
     x2 = 1
    
@@ -75,26 +165,27 @@ while game == 1:
                     x2 = 2
                   
 
+ 
 
-        fenster.fill((255, 175, 0))
+        window.fill((255, 175, 0))
         text_title = pygame.font.SysFont(None, 125)
         text_subt  = pygame.font.SysFont(None, 75)
         text_subt2 = pygame.font.SysFont(None, 25)
      
 
         title = text_title.render("Python Slayer", True, black)
-        fenster.blit(title, (50, 50))
-
+        window.blit(title, (50, 50))
+        window.blit(knight, (100, 200))
         subt1 = text_subt.render("Press 'Space' to start.", True, black, (255, 250, 0))
         subt2 = text_subt.render("Press 'Esc' to exit game.", True, black, (255, 100, 0))
         
-        fenster.blit(subt1, (100, 325))
-        fenster.blit(subt2, (100, 400))
+        window.blit(subt1, (100, 325))
+        window.blit(subt2, (100, 400))
      
         pygame.display.update()
 
     if x2 == 2:
-          game = StartGame(fenster)
+          game = StartGame(window)
 
 pygame.quit()
 
